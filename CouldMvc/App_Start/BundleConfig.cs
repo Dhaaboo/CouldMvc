920b309374108f3ffbdbcff0bbdbd1af0961305c
Dhaaboo <xarwaashe00@hotmail.com>
2017-04-17 16:33:44 -0700

    Layout Design

--- CouldMvc/App_Start/BundleConfig.cs
+++ CouldMvc/App_Start/BundleConfig.cs
@@ -22,9 +22,10 @@ namespace CouldMvc
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
